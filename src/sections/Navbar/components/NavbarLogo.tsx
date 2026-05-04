0c612daffda390257204401f40efab0ef4c08ce0
Mickael78000 <mickael.girondeau@gmail.com>
2026-05-04 14:56:46 +0200

    fix(mobile): supprimer float-left navbar, centrer logo mobile via mx-auto

--- src/sections/Navbar/components/NavbarLogo.tsx
+++ src/sections/Navbar/components/NavbarLogo.tsx
@@ -2,7 +2,7 @@ export const NavbarLogo = () => {
   return (
     <a
       href="/"
-      className="relative text-[15.1297px] font-light items-center box-border caret-transparent flex float-left h-[200px] pt-2 pb-2 justify-center md:justify-start leading-[24.2075px] z-[99] -mt-[57px] md:text-[15.667px] md:h-[220px] md:pt-4 md:pb-4 md:leading-[25.0672px] md:z-auto md:-mt-[57px] gap-x-4"
+      className="relative text-[15.1297px] font-light items-center box-border caret-transparent flex h-[200px] pt-2 pb-2 mx-auto md:mx-0 leading-[24.2075px] z-[99] -mt-[57px] md:text-[15.667px] md:h-[220px] md:pt-4 md:pb-4 md:leading-[25.0672px] md:z-auto md:-mt-[57px] gap-x-4"
     >
       <img
         src="/images/blockhack-logo.webp"
